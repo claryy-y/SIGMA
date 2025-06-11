e91c8673be55d5c3c4ebb89a2007411cd83109f5
claryy-y <shafa.23239@mhs.unesa.ac.id>
2025-06-11 21:44:54 +0700

    ubah title jadi sigma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SIGMA',
+  description: 'Sistem Geospasial Motor Aman',
 }
 
 export default function RootLayout({
